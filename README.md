80eb29dce5457ce325e8eef2c8eebfa9450be60c
Tajjammul <tajjamul.zaman@nxb.com.pk>
2023-03-25 14:51:45 +0500

    test key

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-## MY SHOP
+## MY SHOP Backend
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 
 const appRoutes = require('./src/routes');
 
-const app = express();
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', appRoutes);
-
 
 app.listen(3000);
